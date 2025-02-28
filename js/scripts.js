ead660aa4ad7777e085305fed2a606e81e4b84f5
xjojo132 <xj.remmelink@gmail.com>
2025-02-28 09:25:38 +0100

    eerste dingen

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+let stepX = 10;
+let stepY = 20;
+let contWidth = 400;
+let contHeight = 300;
+let mvDivWidth = 20;
+let mvDivHeight = 20;
+let mvDiv = document.getElementById("moveDiv")
+let contDiv = document.getElementById("container")
+let score = 0;
+
+
+function move() {
+    console.log("in move function");
+    let yPos = mvDiv.offsetTop;
+    console.log("yPos = " + yPos);
+    if (yPos + stepY > 600) {
+        stepY = -stepY
+    }
+    mvDiv.style.top = (yPos + stepY) + "px";
+}
+
+function fStart(){
+    setInterval(move)
+}

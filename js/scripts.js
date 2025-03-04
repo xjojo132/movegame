087053e913af63e5981a12d6eccbd21c325efefe
xjojo132 <xj.remmelink@gmail.com>
2025-03-04 13:12:49 +0100

    tis nu een screen saver met start stop en reset

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,59 @@
-let stepX = 10;
-let stepY = 20;
-let contWidth = 400;
-let contHeight = 300;
+let stepX = 1;
+let stepY = 2;
+let contWidth = 800;
+let contHeight = 600;
 let mvDivWidth = 20;
 let mvDivHeight = 20;
 let mvDiv = document.getElementById("moveDiv")
 let contDiv = document.getElementById("container")
 let score = 0;
-
+let interval
+let started = false;
 
 function move() {
     console.log("in move function");
     let yPos = mvDiv.offsetTop;
     console.log("yPos = " + yPos);
-    if (yPos + stepY > 600) {
+    if (yPos + stepY > 580) {
         stepY = -stepY
+    } else if (yPos + stepY < -5) {
+        stepY = 2;
+        console.log("hallo")
     }
     mvDiv.style.top = (yPos + stepY) + "px";
+
+
+    let xPos = mvDiv.offsetLeft;
+    console.log("xPos = " + xPos);
+    if (xPos + stepX > 780) {
+        stepX = -stepX
+    } else if (xPos + stepX < 0) {
+        stepX = 1;
+        console.log("hallo")
+    }
+    mvDiv.style.left = (xPos + stepX) + "px";
 }
 
-function fStart(){
-    let interval = setInterval(move, 500);
+function fStart() {
+    if (started == false) {
+
+        interval = setInterval(move, 10);
+        started = true;
+    } else {
+
+    }
 }
 
-function fStop(){
+function fStop() {
     clearInterval(interval);
+    started = false;
+}
+
+function reset(){
+    clearInterval(interval);
+    started = false
+    mvDiv.style.left = (10) + "px";
+    mvDiv.style.top = (10) + "px";
+
+
 }
